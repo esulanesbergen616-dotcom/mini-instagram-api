78a974abc7af09911e879c3b1aac420a79bfd638
esulanesbergen616-dotcom <esulanesbergen616@gmail.com>
2026-03-28 11:43:15 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    // Aiven үшін міндетті баптау:
+    host: 'mysql-15344f45-mini-instagram.j.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_gNybVLa9gu6m6y3CvMy',
+    database: 'defaultdb',
+    port: 13574,
     ssl: {
         rejectUnauthorized: false
     }
@@ -18,7 +16,21 @@ db.connect((err) => {
         console.error('MySQL-ге қосылу қатесі:', err.message);
         return;
     }
-    console.log(`Aiven MySQL базасына ${process.env.DB_PORT} порты арқылы сәтті қосылды!`);
+    console.log('Aiven MySQL базасына ТІКЕЛЕЙ сәтті қосылды!');
+
+    const createUsersTable = `
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password_hash VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`;
+
+    db.query(createUsersTable, (err) => {
+        if (err) console.error('Кесте қатесі:', err.message);
+        else console.log('Users кестесі дайын.');
+    });
 });
 
 module.exports = db;
