d9228e651e329fbb06bcf1f46aef0641a8780a3f
esulanesbergen616-dotcom <esulanesbergen616@gmail.com>
2026-04-04 00:40:27 +0500

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 
+const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET || 'your_access_secret_key';
+
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) return res.status(401).json({ message: 'Токен табылмады' });
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Токен жарамсыз' });
+  // Енді біз ACCESS_SECRET айнымалысын қолданамыз
+  jwt.verify(token, ACCESS_SECRET, (err, user) => {
+    if (err) {
+      console.error("JWT тексеру қатесі:", err.message);
+      return res.status(403).json({ message: 'Токен жарамсыз' });
+    }
     req.user = user;
     next();
   });
