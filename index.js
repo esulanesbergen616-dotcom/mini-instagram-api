314b70d4ffd78dcfc4141f518f5adede85cf7dca
esulanesbergen616-dotcom <esulanesbergen616@gmail.com>
2026-05-01 19:25:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const postRoutes = require('./posts');
 const likeRoutes = require('./likes');
 const commentRoutes = require('./comments');
 const followRoutes = require('./follows');
+const adsRouter = require('./ads'); 
 
 const app = express();
+
+// 1. МИНДЕТТІ: Ең алдымен JSON өңдеушіні қосамыз
 app.use(express.json());
 
-// Роутерлер
+// 2. Роутерлер (Барлығы JSON-нан кейін болуы керек)
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes); 
 app.use('/likes', likeRoutes);
 app.use('/comments', commentRoutes);
 app.use('/follows', followRoutes);
+app.use('/api/ads', adsRouter); // Жарнама роутері осында
 
 app.get('/', (req, res) => {
   res.send('Mini-Instagram API жұмыс істеп тұр!');
 });
 
-// ГЛОБАЛДЫҚ ҚАТЕ ӨҢДЕУШІ (Error Handling)
+// 3. Қате өңдеуші (Ең соңында тұрады)
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({
